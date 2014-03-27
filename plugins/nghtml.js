dcc85e348a49c3021a1a8831db9ae2f9346683a3
CamShaft <bytheway.cameron@gmail.com>
2014-03-26 23:41:03 -0600

    use partials instead of angular-templates

--- plugins/nghtml.js
+++ plugins/nghtml.js
@@ -11,7 +11,7 @@ module.exports = function(builder) {
   nghtml({
     webroot: 'public',
     extension: '.jade',
-    confProp: 'angular-templates',
+    confProp: 'partials',
     hook: function (content, filename) {
       var opts = {
         filename: filename
