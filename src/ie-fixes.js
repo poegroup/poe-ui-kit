c5a292d59dd0fbf64a3ca1dea5c8291ce14b0e4c
Benjamin Bertrand <bertrand.design@gmail.com>
2014-05-30 15:07:07 -0600

    Update ie-fixes.js
    Adding another punchline,..

--- src/ie-fixes.js
+++ src/ie-fixes.js
@@ -11,6 +11,7 @@
     var pathname = window.location.pathname + '';
     var path = base === '/' ? base : pathname.replace(base, '');
     if (path === '') path = '/';
+    if (base.charAt(base.length-1) !== '/') base += '/';
     window.location.replace(base + '#!' + path + window.location.search + window.location.hash);
   }
 })();
